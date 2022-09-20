17f141dc3ace2b548ef38809ccc504c93023ac73
SantiRosso <santiagorosso@live.com.ar>
2022-09-20 00:02:55 -0300

    AlgoritmosII finished

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,22 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
+  let pivot = array[Math.floor(Math.random() * array.length)];
+  let left = [];
+  let equals = [];
+  let right = [];
 
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] < pivot) {
+      left.push(array[i]);
+    } else if(array[i] === pivot) {
+      equals.push(array[i]);
+    } else {
+      right.push(array[i])
+    }
+  }
+  return [].concat(quickSort(left),equals,quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +29,29 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length === 1) return array;
+  var mitad = Math.floor(array.length / 2);
+  var left = array.slice(0, mitad);
+  var right = array.slice(mitad);
 
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right){
+  var izq = 0;
+  var der = 0;
+  var arrayMergeado = [];
+
+  while (izq < left.length && der < right.length) {
+    if(left[izq] < right[der]) {
+      arrayMergeado.push(left[izq]);
+      izq++;
+    } else {
+      arrayMergeado.push(right[der]);
+      der++
+    }
+  }
+  return arrayMergeado.concat(left.slice(izq)).concat(right.slice(der));
 }
 
 // No modificar nada debajo de esta línea
